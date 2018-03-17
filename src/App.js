6557bed3d3d05c51c5e7d1a67fc722dde321248d
Bill <billh93@gmail.com>
2018-03-16 21:07:31 -0500

    Added NPM Package: 'semantic-ui-react'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import CssBaseline from 'material-ui/CssBaseline';
-import Grid from 'material-ui/Grid';
 import GMap from './map.js';
 import SearchContainer from './searchContainer.js';
 import './App.css';
@@ -8,15 +6,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-        <Grid container s={12} className="App">
-            <CssBaseline />
-            <Grid item l={4} className="SearchContainer">
-                <SearchContainer />
-            </Grid>
-            <Grid item l={8} className="GMapContainer">
-                <GMap />
-            </Grid>
-        </Grid>
+        <div className="App">
+            <GMap />
+        </div>
     );
   }
 }
--- src/searchContainer.js
+++ src/searchContainer.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
-import AppBar from "material-ui/AppBar";
-import Input from "material-ui/Input";
 
 export default class SearchContainer extends Component {
   render() {
     return (
-        <AppBar position="static">
-          <Input
-            placeholder="Search"
-            inputProps={{
-              "aria-label": "Description"
-            }}
-          />
-        </AppBar>
+        <h1>Test</h1>
     );
   }
 }
